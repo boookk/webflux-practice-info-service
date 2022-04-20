cdd41fbd492a503dae29f1b4981f5ee056b4051f
boookk <mint49579>
2022-04-20 16:19:18 +0900

    init

---
+++ src/main/java/com/boookk/webfluxpracticeinfoservice/WebfluxPracticeInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.boookk.webfluxpracticeinfoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxPracticeInfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxPracticeInfoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boookk/webfluxpracticeinfoservice/WebfluxPracticeInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boookk.webfluxpracticeinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxPracticeInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
